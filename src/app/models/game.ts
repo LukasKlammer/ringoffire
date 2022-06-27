23a6f76dc29c645ccdc77928285734b94ad7d759
unknown <klammer.lukas@hotmail.com>
2022-06-27 07:21:09 +0200

    render all users, change user image

--- src/app/models/game.ts
+++ src/app/models/game.ts
@@ -1,5 +1,5 @@
 export class Game {
-  public players:string[] = [];
+  public players:string[] = ['Lukas', 'Janine', 'Alexander'];
   public stack:string[] = [];
   public playedCards:string[] = [];
   public currentPlayer = 0;
